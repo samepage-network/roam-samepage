338a719d22be24105f78b669be6350065629633f
David Vargas <dvargas92495@gmail.com>
2022-04-07 17:07:49 -0400

    500 minutes per dollar

--- lambdas/sendmessage.ts
+++ lambdas/sendmessage.ts
@@ -107,7 +107,7 @@ const messageGraph = ({
         .then((r) =>
           r.Item
             ? r.Item.user
-              ? meterRoamJSUser(r.Item?.user?.S)
+              ? meterRoamJSUser(r.Item?.user?.S, 5)
               : Promise.reject(
                   new Error(
                     `How did non-authenticated client try to send message from ${sourceGraph} to ${graph}?`
@@ -360,7 +360,7 @@ const dataHandler = async (
                 },
               })
               .promise()
-              .then(() => meterRoamJSUser(user, 100))
+              .then(() => meterRoamJSUser(user, 500))
               .catch(
                 emailCatch(
                   `Failed to meter Multiplayer user for network ${name}`
