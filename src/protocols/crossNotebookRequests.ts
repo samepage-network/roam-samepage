e0c7ff2bd617b32110fa49d1aa10e2666ec8a6d7
David Vargas <dvargas92495@gmail.com>
2023-04-03 17:28:16 -0400

    pull out getDatalogQuery for reuse between the API and src

--- src/protocols/crossNotebookRequests.ts
+++ src/protocols/crossNotebookRequests.ts
@@ -1,17 +1,20 @@
 // THIS FILE IS UNDER HEAVY DEVELOPMENT
 // We are working on the new protocol for cross-notebook requests.
-import getAllPageNames from "roamjs-components/queries/getAllPageNames";
-import type { SamePageAPI } from "samepage/internal/types";
+import type { json, SamePageAPI } from "samepage/internal/types";
+import getDatalogQuery, { datalogArgsSchema } from "../utils/getDatalogQuery";
 import calculateState from "../utils/calculateState";
 
 const crossNotebookRequests = (api: SamePageAPI) => {
   const removeListener = api.addNotebookRequestListener(
     async ({ request, sendResponse }) => {
       if (Array.isArray(request.conditions)) {
+        const query = getDatalogQuery(datalogArgsSchema.parse(request));
+        console.log("query", query);
+        const results = (
+          window.roamAlphaAPI.data.fast.q(query) as [json][]
+        ).map((r) => r[0]);
         sendResponse({
-          results: getAllPageNames()
-            .slice(0, 10)
-            .map((id) => ({ id })),
+          results,
         });
       } else if (typeof request.notebookPageId === "string") {
         const pageData = await calculateState(request.notebookPageId);
