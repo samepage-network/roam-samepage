1ccdd8f3da2f92b1f9d8a764a794aa1724f308c5
David Vargas <dvargas92495@gmail.com>
2023-04-07 14:35:48 -0400

    2.7.5

--- src/protocols/crossNotebookRequests.ts
+++ src/protocols/crossNotebookRequests.ts
@@ -1,14 +1,20 @@
 // THIS FILE IS UNDER HEAVY DEVELOPMENT
 // We are working on the new protocol for cross-notebook requests.
 import type { json, SamePageAPI } from "samepage/internal/types";
-import getDatalogQuery, { datalogArgsSchema } from "../utils/getDatalogQuery";
+import getDatalogQuery, {
+  samePageQueryArgsSchema,
+} from "../utils/getDatalogQuery";
 import calculateState from "../utils/calculateState";
+import compileDatalog from "../utils/compileDatalog";
 
 const crossNotebookRequests = (api: SamePageAPI) => {
   const removeListener = api.addNotebookRequestListener(
     async ({ request, sendResponse }) => {
       if (Array.isArray(request.conditions)) {
-        const query = getDatalogQuery(datalogArgsSchema.parse(request));
+        const result = samePageQueryArgsSchema.safeParse(request);
+        if (!result.success) return;
+        const datalogQuery = getDatalogQuery(result.data);
+        const query = compileDatalog(datalogQuery);
         const results = (
           window.roamAlphaAPI.data.fast.q(query) as [json][]
         ).map((r) => r[0]);
